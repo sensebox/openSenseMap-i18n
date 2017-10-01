bc743621350c1996352fe7cdeb3fcc62a7123de3
Matthias Pfeil <matthias.pfeil@wwu.de>
2017-10-01 14:06:27 +0200

    build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,58 @@
+var fs = require('fs');
+
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var merge = require('gulp-merge-json');
 var jsonminify = require('gulp-jsonminify');
+var jsonschema = require('gulp-json-schema');
 
 // Get files from subdirectories
-function getFiles () {
-
+function getFiles (dir, files_){
+  files_ = files_ || [];
+  var files = fs.readdirSync(dir);
+  for (var i in files){
+    var name = dir + '/' + files[i];
+    if (fs.statSync(name).isDirectory()){
+      getFiles(name, files_);
+    } else {
+      if (!files_.includes(files[i])) {
+        files_.push(files[i]);
+      }
+    }
+  }
+  return files_;
 }
+
+// Clean up tmp folder
+gulp.task('clean', function () {
+  return gulp.src('.tmp')
+    .pipe(clean());
+});
 
 // Merges all language files from subdirectories
 gulp.task('merge', function(){
-  return gulp.src('src/*/de_DE.json')
-    .pipe(merge({
-      fileName: 'de_DE.json'
-    }))
-    .pipe(gulp.dest('tmp/'));
+  var files = getFiles('src');
+
+  return files.map(function (file) {
+    return gulp.src(`src/*/${file}`)
+      .pipe(merge({
+        fileName: `${file}`
+      }))
+      .pipe(gulp.dest('.tmp/'));
+  });
 });
 
 // Lints merged files and checks if the required keys
 // are available.
-gulp.task('lint', function () {
-
+gulp.task('validate', function () {
+  return gulp.src('.tmp/*.json')
+    .pipe(jsonschema('src/schema.json'));
 });
 
-gulp.task('minify', ['merge'], function () {
-  return gulp.src(['tmp/*.json'])
+gulp.task('minify', ['validate'], function () {
+  return gulp.src(['.tmp/*.json'])
     .pipe(jsonminify())
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', [ 'merge', 'minify' ]);
+gulp.task('build', ['merge', 'minify' ]);
