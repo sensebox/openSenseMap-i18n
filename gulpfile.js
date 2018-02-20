68252608a12bc0b95838de22f63402e9853f7808
ubergesundheit <ubergesundheit@googlemail.com>
2018-02-20 16:03:59 +0100

    build(gulp): Filter out _API keys from source files

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,8 @@ var jsonschema = require('gulp-json-schema');
 
 var through = require('through2');
 
+var PluginError = gutil.PluginError;
+
 // Get files from subdirectories
 function getFiles (dir, files_){
   files_ = files_ || [];
@@ -27,6 +29,31 @@ function getFiles (dir, files_){
   return files_;
 }
 
+// Creating a stream through which each file will pass
+const gulpAPIKeyRemover = through.obj(function(file, enc, cb) {
+  if (file.isNull()) {
+    // return empty file
+    return cb(null, file);
+  }
+
+  if (file.isStream()) {
+    this.emit('error', new PluginError('gulp-apikeyremover', 'Streaming not supported'));
+    return cb();
+  }
+
+  if (file.isBuffer()) {
+    const obj = JSON.parse(file.contents);
+
+    Object.keys(obj)
+      .filter(k => k.endsWith("_API"))
+      .forEach(k => delete obj[k]);
+
+    file.contents = Buffer.from(JSON.stringify(obj));
+  }
+
+  cb(null, file);
+});
+
 // Clean up tmp folder
 gulp.task('clean', function () {
   return gulp.src('dist')
@@ -39,6 +66,7 @@ gulp.task('merge', ['clean'], function(){
 
   return files.map(function (file) {
     return gulp.src(`src/*/${file}`)
+      .pipe(gulpAPIKeyRemover)
       .pipe(merge({
         fileName: `${file}`
       }))
